8554d2997a2452f280ad34bbb2ce315c5990b560
Min Wu <mw@minreklame.dk>
2013-09-10 14:44:16 +0200

    change command start to use init.d full path then service

--- zConfig/autostart.sh
+++ zConfig/autostart.sh
@@ -10,7 +10,7 @@ if [ -f $foreverPidFile ]
 then
       echo "Forever is running."
 else
-      service nodecdn start
+      /etc/init.d/nodecdn start
       echo "Forever is down."
       echo "($(date)) -- Forever is down." >> /web/log/node.log
 fi
